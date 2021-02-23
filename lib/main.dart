13bd9d74e0f38dae40aed7bc0a422d1bcdc5b7d4
gonpress <gonpress@naver.com>
2021-02-23 23:00:22 +0900

    77page

--- lib/main.dart
+++ lib/main.dart
@@ -4,62 +4,46 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyApp();
+  }
+}
+
+class _MyApp extends State<MyApp> {
+  var switchValue = false;
+  String test = 'hello';
+  Color _color = Colors.blue;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: Text('Hello\nFlutter'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        darkTheme: ThemeData.light(),
+        home: Scaffold(
+          body: Center(
+              child: RaisedButton(
+            child: Text('$test'),
+            color: _color,
+            onPressed: () {
+              if (_color == Colors.blue) {
+                setState(() {
+                  test = 'flutter';
+                  _color = Colors.amber;
+                });
+              } else {
+                setState(() {
+                  test = 'flutter';
+                  _color = Colors.blue;
+                });
+              }
+            },
+          )),
+        ));
   }
 }
